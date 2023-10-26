19af4ec8078a0db1b189f988002d087675746042
Sivko_nb88@top-academy.ru <Sivko_nb88@top-academy.ru>
2023-10-26 21:44:11 +0300

    Shift DONE

--- Shift/main.cpp
+++ Shift/main.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 using namespace std;
 #define tab "\t"
+//#define LEFT
+#define RIGHT
 
 /*
 ---------------------------------------------------------------
@@ -35,14 +37,14 @@ void main()
 	std::cout << endl;
 
 
-
+#ifdef LEFT
 
 	//Циклический сдвиг массива влево
 
 
-	int number_of_shift;
-	cout << "Введите количество сдвигов" << endl; cin >> number_of_shift;
-	for (int i = 0; i < number_of_shift; i++)
+	int number_of_shifts;
+	cout << "Введите количество сдвигов влево: " << endl; cin >> number_of_shifts;
+	for (int i = 0; i < number_of_shifts; i++)
 	{
 		int buffer = arr[0];
 		for (int i = 0; i < n; i++)
@@ -52,24 +54,22 @@ void main()
 		arr[n - 1] = buffer;
 	}
 
-
-	//Циклический сдвиг массива вправо
-	int number_of_shift;
-	cout << "Введите количество сдвигов" << endl; cin >> number_of_shift;
-	for (int i = 9; i < number_of_shift; i++)
+#endif //LEFT(CLASSWORK)
+#ifdef RIGHT	
+	int number_of_shifts; //Создаем переменную которая будет хранить количество сдвигов
+	cout << "Введите количество сдвигов вправо: "; cin >> number_of_shifts; //Вводим кол-во с клавиатуры
+	for (int i = 0; i < number_of_shifts; i++)
 	{
-		int buffer = arr[9];
-		for (int i = 0; i < n; i++)
+		int buffer = arr[n - 1];
+		for (int i = n - 1; i > 0; i--)
 		{
 			arr[i] = arr[i - 1];
 		}
-		arr[n + 1] = buffer;
+		arr[0] = buffer;
 	}
 
 
-
-
-	
+#endif //RIGHT(HOMEWORK)
 	//Вывод сдвинутого массива на экран
 	for (int i = 0; i < n; i++) 
 	{
